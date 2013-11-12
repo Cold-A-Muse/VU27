f069f36589712ee72f2a3a2958a33d0d9eb166c0
Daan Helsloot <daan.998@hotmail.com>
2013-11-13 00:34:37 +0100

    Made my own factorial function

--- chapter3/Pizza/Pizza.py
+++ chapter3/Pizza/Pizza.py
@@ -3,11 +3,18 @@ __author__ = 'Helsloot'
 import math
 
 
-def combinaties(n, k):
-    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+def factorial(number):
+    product = 1
+    for i in range(number):
+        product = (product * (i + 1))
+    return product
 
-mario = combinaties(10, 3)
-luigi = combinaties(9, 4)
+
+def combinations(n, k):
+    return factorial(n) / (factorial(k) * factorial(n - k))
+
+mario = combinations(10, 3)
+luigi = combinations(9, 4)
 
 print("Mario can make %d pizzas." % mario)
 print("Luigi is able to make %d pizzas." % luigi)
