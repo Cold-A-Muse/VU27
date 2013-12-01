6320e9fe1e76dac3d3917272aff1395320d84df4
Daan Helsloot <daan.998@hotmail.com>
2013-12-01 13:40:15 +0100

    Need to read the different lines and put them in different CoordinateRows

--- Chapter5/Pirate/Pirate.py
+++ Chapter5/Pirate/Pirate.py
@@ -21,6 +21,13 @@ def removeEqual(coord_row, coordinate_row_instance):
             coordinate_row_instance.add(coord_row[i])
 
 
+def splitEqual(coord_row):
+    input_split = coord_row.split('=')
+    for i in input_split:
+        i = Coordinate(i)
+        i.getAdjustedX()
+        return i
+
 def flatten(lst):
     return sum(([x] if not isinstance(x, list) else flatten(x)for x in lst), [])
 
@@ -29,14 +36,21 @@ f = file_input()
 content = f.splitlines()
 coords = [i.split() for i in content]
 print coords
+
 cr = CoordinateRow()
 addAll(coords[0], cr)
-cr2 = CoordinateRow()
 print cr.coordinate_row
+
+cr2 = CoordinateRow()
 removeEqual(coords[0], cr2)
 print cr2.coordinate_row
 print flatten(cr2.coordinate_row)
+cr3 = CoordinateRow()
 for i in flatten(cr2.coordinate_row):
-    print str(Coordinate(i).getAdjustedX())+","+str(Coordinate(i).getY()),
+    cr3.add(str(Coordinate(i).getAdjustedX())+","+str(Coordinate(i).getY()))
+print cr3.coordinate_row
 
+cr4 = CoordinateRow()
+split_eq = splitEqual(f)
+print split_eq.coord
 
